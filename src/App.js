4f36556bdd1408e8ad746a3c6bbf3e600e0c7a26
conbec <56614504+conbec@users.noreply.github.com>
2020-01-24 20:57:39 -0800

    color changinf and array getting mutated

--- src/App.js
+++ src/App.js
@@ -8,17 +8,32 @@ function App() {
     [false, false, false],
     [false, false, false]
   ]);
+  
+  const toggleCell = ({ ev, cell, row, col }) => {
+
+    if (cell === false) { //need to test on cell, not classList once cells is resetting
+      ev.target.classList.add('colored')
+      cells[row][col] = true;
+    }
+    else {
+      ev.target.classList.remove('colored')
+      cells[row][col] = false;
+    }
+    setCells([...cells])
+    console.log(cells);
+    console.log(cell);
+  }
 
   return (
-    <div className="table">
+    <div className="table"> 
       <div className="row">
-        {cells[0].map((cell, idx) => { return <div key={idx} className="box" onClick={cell === false ? setCells(true) : setCells(false)}></div> })};
+        {cells[0].map((cell, idx) => { return <div key={idx} className="box" onClick={(ev)=> { toggleCell({ev, cell, row: 0, col: idx}) }}></div> })}
       </div>
       <div className="row">
-        {cells[1].map((cell, idx) => { return <div key={idx} className="box" onClick={cell === false ? setCells(true) : setCells(false)}></div> })};
+        {cells[1].map((cell, idx) => { return <div key={idx} className="box" onClick={(ev) => { toggleCell({ ev, cell, row: 1, col: idx })}}></div> })}
       </div>
       <div className="row">
-        {cells[2].map((cell, idx) => { return <div key={idx} className="box" onClick={cell === false ? setCells(true) : setCells(false)}></div> })};
+        {cells[2].map((cell, idx) => { return <div key={idx} className="box" onClick={(ev) => { toggleCell({ ev, cell, row: 2, col: idx })}}></div> })}
       </div>
     </div>
   );
